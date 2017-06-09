f60b0b15d2ef7290fedf5946809b518a46a1d41f
Harrison Carter <harrycarter1192@gmail.com>
2017-06-09 01:29:47 -0500

    client/server modules; one-way chat

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 import options
 import input
+import server, client
 
 if __name__ == "__main__":
-	opts = options.getOpts()
-	input.reading()
-	options.printOpts(opts)
+
+	# Read command line options and arguments
+	# terminates if usage is incorrect
+	opts, args = options.getOptsArgs()
+	
+	# Store port number/server address
+	portNum = args[0]
+	serverAddr = 'localhost'
+	if (len(args) == 2):
+		serverAddr = args[1]
+	
+	# Determine Server or Client
+	if opts:
+		print("\nMESSENGER SERVER")
+		print("	- Port Number: " + str(portNum))
+		print("	- Server Address: " + str(serverAddr))
+		# Setup Server
+		sock, addr = server.setup(serverAddr, portNum)
+		# Display connection details
+		# server.getDetails(sock, addr)
+		# Input and display messages
+		server.receive(sock)
+		# Close socket
+		server.end(sock)
+	else:
+		print("\nMESSENGER CLIENT")
+		print("	- Port Number: " + str(portNum))
+		print("	- Server Address: " + str(serverAddr))
+		# Setup Client
+		sock = client.setup(serverAddr, portNum)
+		# Display connection details
+		# client.getDetails(sock)
+		# Send messages until standard input terminates
+		client.sendMessage(sock)
